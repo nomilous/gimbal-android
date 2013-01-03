483490b701a8afc604d9e73e4588d39d6f1abbdf
richardemslie <richard@clue.co.za>
2013-01-03 23:59:20 +0200

    scan QRcode

--- src/nomilous/gimbal/Gimbal.java
+++ src/nomilous/gimbal/Gimbal.java
@@ -3,13 +3,59 @@ package nomilous.gimbal;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.widget.LinearLayout;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
 public class Gimbal extends Activity {
+
+    private LinearLayout viewports;
+    private TextView instruction;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gimbal);
 
+        final Activity scanResultHandler = this;
+
+        viewports = (LinearLayout) findViewById(R.id.viewports);
+        
+        instruction = (TextView) findViewById(R.id.instruction);
+        instruction.setText("touch to grab viewport");
+
+        viewports.setOnClickListener( new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                IntentIntegrator qrScan = new IntentIntegrator(scanResultHandler);
+                qrScan.initiateScan();
+
+            }
+
+        });
+
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+  
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        
+        if (scanResult != null) {
+            
+            instruction.setText(scanResult.getContents());
+
+        }
+  
+    }
+
 }
