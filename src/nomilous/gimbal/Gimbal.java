de6e45128bade228c79cc3d5dbd72add9bd7ac65
richardemslie <richard@clue.co.za>
2013-01-03 20:42:26 +0200

    fullscreen, no title

---
+++ src/nomilous/gimbal/Gimbal.java
@@ -0,0 +1,15 @@
+package nomilous.gimbal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Gimbal extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gimbal);
+
+    }
+}
