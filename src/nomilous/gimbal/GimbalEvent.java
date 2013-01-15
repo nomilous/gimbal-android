45daf178da1324175b14be9b08a42fbbfc89198c
richardemslie <richard@clue.co.za>
2013-01-15 23:59:26 +0200

    pending multiple subscribers

--- src/nomilous/gimbal/GimbalEvent.java
+++ src/nomilous/gimbal/GimbalEvent.java
@@ -1,5 +1,7 @@
 package nomilous.gimbal;
 
+import java.util.Hashtable;
+
 public class GimbalEvent {
 
     public static class Touch {
@@ -12,9 +14,27 @@ public class GimbalEvent {
 
     }
 
-    public static interface Listener {
+    public static interface Subscriber {
 
         public abstract void onTouchEvent( GimbalEvent.Touch event );
+
+    }
+
+
+    public static class Publisher {
+
+        private Integer idSequence = 0;
+
+        private Hashtable<Integer,Subscriber> subscribers = 
+            new Hashtable<Integer,Subscriber>();
+
+        public synchronized Integer subscribe(Subscriber subscriber) {
+
+            Integer id = idSequence++;
+            subscribers.put(id, subscriber);
+            return id;
+
+        }
 
     }
 
--- src/nomilous/gimbal/GimbalGL10Renderer.java
+++ src/nomilous/gimbal/GimbalGL10Renderer.java
@@ -4,6 +4,6 @@ import android.opengl.GLSurfaceView;
 
 public abstract class GimbalGL10Renderer
     implements GLSurfaceView.Renderer,
-               GimbalEvent.Listener {
+               GimbalEvent.Subscriber {
 
 }
