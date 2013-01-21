78eb424e255c98531524fb60093f9709abdd3f9a
richardemslie <richard@clue.co.za>
2013-01-21 19:33:49 +0200

    logtag

--- src/nomilous/Util.java
+++ src/nomilous/Util.java
@@ -8,37 +8,39 @@ import android.view.Gravity;
 
 public class Util {
 
+    public static String tag = "GIMBAL";
+
     //
     // $ adb logcat | grep =====
     //
 
     public static void todo( String message ) {
 
-        Log.i( "=====TODO", message );
+        Log.i( tag + "::TODO", message );
 
     }
 
     public static void debug( String message ) {
 
-        Log.d( "=====DEBUG", message );
+        Log.d( tag + "::DEBUG", message );
 
     }
 
     public static void info( String message ) {
 
-        Log.i( "=====INFO", message );
+        Log.i( tag + "::INFO", message );
 
     }
 
     public static void warn( String message ) {
 
-        Log.w( "=====WARN", message );
+        Log.w( tag + "::WARN", message );
 
     }
 
     public static void error( String message ) {
 
-        Log.e( "=====ERROR", message );
+        Log.e( tag + "::ERROR", message );
 
     }
 
